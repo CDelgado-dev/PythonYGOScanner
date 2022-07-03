63fb47a44f64098c67c4f71f0ac5ade9ae0d3ce0
Todd Thomas <ddotthomas@github.com>
2022-07-03 17:56:34 -0600

    card lookup prototype

--- YGOAPI.py
+++ YGOAPI.py
@@ -5,13 +5,16 @@ import json
 def main():
     response = (requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")).json()
     data = response['data']
-    print("Type:", type(response))
+    #print("Type:", type(response))
     #items in data request pull
+    card = input("Card to lookup: ")
     for x in data:
-        print(x)
+        if x['name'] == "card":
+            print(x)
         #print("Name of Card: %s" % (x['name']))
         #print("\n")
         
+
 
     #print("ID: ", response['data'] )
 
