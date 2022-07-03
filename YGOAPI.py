a90d3a27468885bc291d6062f77b334dffcacc12
Cesar Delgado <CDelgado-dev@github.com>
2022-07-03 17:44:59 -0600

    new change - step through request

--- YGOAPI.py
+++ YGOAPI.py
@@ -3,8 +3,17 @@ import json
 
 ##test response not final
 def main():
-    response = requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-    print(type(response))
+    response = (requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")).json()
+    data = response['data']
+    print("Type:", type(response))
+    #items in data request pull
+    for x in data:
+        print(x)
+        #print("Name of Card: %s" % (x['name']))
+        #print("\n")
+        
+
+    #print("ID: ", response['data'] )
 
 if __name__ == '__main__':
     main()
