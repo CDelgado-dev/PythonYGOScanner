4050624bb5242adba3ad6f3747c15db7518d3b31
Todd Thomas <ddotthomas@github.com>
2022-07-03 18:39:23 -0600

    Cleanup comments

--- YGOAPI.py
+++ YGOAPI.py
@@ -5,19 +5,16 @@ import json
 def main():
     response = (requests.get("https://db.ygoprodeck.com/api/v7/cardinfo.php")).json()
     data = response['data']
-    #print("Type:", type(response))
     #items in data request pull
     while True:
+        #Start an infinite loop, keep the website's api in memory
         card = input("Card to lookup: ")
         for cardDict in data:
             if cardDict['name'] == card:
                 print(f"Name: {cardDict['name']} Price: {cardDict['card_prices'][0]['tcgplayer_price']}")
-        if not input("Lookup another card? [Yes] or [No]: ").lower().strip().startswith('y'):
+        #After displaying card info based on card's name, ask if they want to lookup another card
+        if input("Lookup another card? [Yes] or [No]: ").lower().strip().startswith('n'):
             break
-        #print("Name of Card: %s" % (x['name']))
-        #print("\n")
-
-    #print("ID: ", response['data'] )
 
 if __name__ == '__main__':
     main()
