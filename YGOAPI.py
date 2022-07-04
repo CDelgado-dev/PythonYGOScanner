c7c3705b0d78ccd73d807c279a10d3a1048afa95
Todd Thomas <ddotthomas@github.com>
2022-07-03 18:36:12 -0600

    Added user input, displays card name and TCGPlayer price

--- YGOAPI.py
+++ YGOAPI.py
@@ -7,14 +7,15 @@ def main():
     data = response['data']
     #print("Type:", type(response))
     #items in data request pull
-    card = input("Card to lookup: ")
-    for x in data:
-        if x['name'] == "card":
-            print(x)
+    while True:
+        card = input("Card to lookup: ")
+        for cardDict in data:
+            if cardDict['name'] == card:
+                print(f"Name: {cardDict['name']} Price: {cardDict['card_prices'][0]['tcgplayer_price']}")
+        if not input("Lookup another card? [Yes] or [No]: ").lower().strip().startswith('y'):
+            break
         #print("Name of Card: %s" % (x['name']))
         #print("\n")
-        
-
 
     #print("ID: ", response['data'] )
 
