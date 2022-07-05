f0d5527d53ac64d7209c0a32c3c8d5cea575bc83
Todd Thomas <ddotthomas@github.com>
2022-07-05 08:16:13 -0600

    Added set lookup

--- YGOAPI.py
+++ YGOAPI.py
@@ -4,13 +4,22 @@ import json
 ##test response not final
 def main():
     card = input("Card: ")
-    response = (requests.get(f"https://db.ygoprodeck.com/api/v7/cardinfo.php?name={card}")).json()
+    response = (requests.get(f"https://db.ygoprodeck.com/api/v7/cardinfo.php?name={card}")).json()  #My next idea is to grab all the cards info at the beginning again and let the user choose to lookup another card without grabbing data from the website
     data = response['data'][0]
     print(data['name'])
     #If a card exist in more than one set, prompt the user to choose a set, otherwise just display the one set and its price
     if len(data['card_sets']) > 1:
-        print("more than one")
+        print("This card has more than one set, you can select which set using the leading number (ie 00)\nor with the set code. Otherwise just hit Enter to see all")
         setList = data['card_sets']
+
+        for cardSetDict in range(len(setList)):  #Displays the set key name to user to allow them to select one in the lookupset variable
+            print(f"  {cardSetDict} -=- Set Code: {setList[cardSetDict]['set_code']} Set Name: {setList[cardSetDict]['set_name']}")
+        lookupset = input(f"Number [0-{len(setList) - 1}] or Set Code or just hit <Enter> to see all: ").strip() #displays 0-34 for example to help the user, will also accept exact set codes, since user has that info on card
+        for cardSet in range(len(setList)):  #Lil reminder setList is a list variable, I thought it was also a dictionary and was trying to interact with it the wrong way
+            if lookupset == str(cardSet) or lookupset == setList[cardSet]['set_code']:
+                print(f"Set Name: {setList[cardSet]['set_name']} Set Price: {setList[cardSet]['set_price']} Set Code: {setList[cardSet]['set_code']} Set Rarity: {setList[cardSet]['set_rarity']}")
+            if not lookupset: #If the user didn't input anything and just hit enter, display all
+                print(f"{cardSet}")
     else:
         cardSet = data['card_sets'][0]
         print(f"Set Name: {cardSet['set_name']} Set Code: {cardSet['set_code']} Set Price: {cardSet['set_price']}")
